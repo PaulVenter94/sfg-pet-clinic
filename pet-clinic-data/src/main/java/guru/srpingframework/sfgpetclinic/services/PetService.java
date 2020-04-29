95d0f1bc833c9da06e27d3e0cf5353985c0ff561
Paul <paulventer94@gmail.com>
2020-04-29 20:45:05 +0300

    Added interfaces

---
+++ pet-clinic-data/src/main/java/guru/srpingframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package guru.srpingframework.sfgpetclinic.services;
+
+import guru.srpingframework.sfgpetclinic.model.Owner;
+import guru.srpingframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+}
